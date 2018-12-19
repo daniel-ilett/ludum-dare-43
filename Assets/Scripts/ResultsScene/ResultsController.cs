5e41c20f1f1aada29971bdc95988b36f6767ee09
Daniel Ilett <danielilett@gmail.com>
2018-12-19 12:16:09 +0000

    Small changes to ResultsController.

--- Assets/Scripts/ResultsScene/ResultsController.cs
+++ Assets/Scripts/ResultsScene/ResultsController.cs
@@ -69,7 +69,7 @@ public class ResultsController : MonoBehaviour
 			yield return null;
 		}
 		
-		yield return new WaitForSeconds(5.0f);
+		yield return new WaitForSeconds(10.0f);
 		StartCoroutine(FadeOut());
 	}
 
